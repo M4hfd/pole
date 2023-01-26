d6fe9af9a16608552a98b0121fd026cda7fc4688
M4hfd <83091151+M4hfd@users.noreply.github.com>
2023-01-26 16:28:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ words.append(secretword)
 print()
 print("\033[2J")
 win = False
-list = []
+all_letters = []
 true_letters = []
 points = [0, 0, 0]
 counter = 1
 
 while win == False:
     letter = input("\nИгрок " + str(counter) + " введите букву: ")  
-    if letter in list:
+    if letter in all_letters:
         print("Буква " + letter + " уже была названа")
     elif letter in secretword:
         print("Вы угадали букву: " + letter)
@@ -27,16 +27,15 @@ while win == False:
             print("Счет: " + str(points[2]))
     else:
         print("Буквы " + letter +  " нет в слове")
-    list.append(letter)
+    all_letters.append(letter)
     for symbol in secretword:
         if symbol in true_letters:
            print(symbol, end="")
         else:
             print("*", end="")
-    if counter == 1 or 2:
-        counter += 1
+    counter += 1
     if counter == 4:
-        counter -= 3
+        counter = 1
     if set(true_letters) == set(secretword):
         win = True
         print("\nСчет игроков: \n Игрок 1: " + str(points[0]) + "\n Игрок 2: " + str(points[1]) + "\n Игрок 3: " + str(points[2]))
